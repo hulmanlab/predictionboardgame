55aaf65fe0b7b72b88bfbf660fb9450ede5b07aa
Anders Aasted Isaksen <aasted87@gmail.com>
2025-01-24 13:10:17 +0100

    Separated setup script from main

--- R/main.R
+++ R/main.R
@@ -1,40 +1,5 @@
-# Check dependencies ----------------------------------------------------
-
-# Define the list of required packages
-required_packages <- c(
-  "ggplot2",
-  "dplyr",
-  "here",
-  "randomNames",
-  "cowplot",
-  "ggplotify",
-  "finalfit",
-  "patchwork"
-)
-
-# Check if any are missing
-for (pkg in required_packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    print(paste("You need to install this packages: ", pkg))
-  } else {
-    print(paste("This package was successfully detected: ", pkg))
-  }
-}
-
-# Install dependencies ----------------------------------------------------
-
-# Check if each package is installed, and install if missing
-for (pkg in required_packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
-  }
-}
-
-
-# Load packages into namespace -------------------------------------------------------
-library(ggplot2)
-library(dplyr)
-library(patchwork)
+# Install and load dependencies -------------------------------------------
+source(here::here("R", "setup.R"))
 
 
 # Create clinical data ----------------------------------------------------
